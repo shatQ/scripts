0cd0d49b214d02e0e9471ebffa93ad6b006e4561
Pawel Szatkowski <pszatkowski.byd@gmail.com>
2020-09-25 20:42:05 +0200

    Reformating code.

--- lock-pkg.sh
+++ lock-pkg.sh
@@ -5,7 +5,7 @@ set -e
 pkg_manager=""
 
 show_help() {
-    cat << EOF 
+    cat <<EOF
 Usage: $(basename $0) [OPTION...] PACKAGES...
 
 Options:
@@ -27,27 +27,27 @@ is_url() {
     else
         return 1
     fi
-        		
+
 }
 
 curl_url() {
-	# Check http header to see if url gives 200 response.
-	http_response=$(curl --silent --head --request GET $1 | head -1)
-	if ! echo $http_response | grep -qi 200; then
-		echo "error: $http_response" >&2
-		exit 1
-	else
-	    echo $(curl $1)
-	fi
+    # Check http header to see if url gives 200 response.
+    http_response=$(curl --silent --head --request GET $1 | head -1)
+    if ! echo $http_response | grep -qi 200; then
+        echo "error: $http_response" >&2
+        exit 1
+    else
+        echo $(curl $1)
+    fi
 }
 
 run_cmd() {
-	cmd=$1
-	args=${@: 2}
+    cmd=$1
+    args=${@:2}
 
     rc=0
-	result=$(eval "$cmd" "$args" 2>&1) || rc=$?
-	while read line; do echo "$cmd: '${line}'"; done <<< "$result"
+    result=$(eval "$cmd" "$args" 2>&1) || rc=$?
+    while read line; do echo "$cmd: '${line}'"; done <<<"$result"
     if [[ $rc -ne 0 ]]; then
         echo "error: $cmd non-zero exit code [$rc]"
         exit 1
@@ -55,7 +55,7 @@ run_cmd() {
 }
 
 check_distro() {
-    distro=$( ( lsb_release -ds || cat /etc/*release ) 2>/dev/null | head -n1)
+    distro=$( (lsb_release -ds || cat /etc/*release) 2>/dev/null | head -n1)
     if echo $distro | grep -qi ubuntu || echo $distro | grep -qi debian; then
         pkg_manager="apt"
     elif echo $distro | grep -qi 'red hat' || echo $distro | grep -qi centos || echo $distro | grep -qi amazon; then
@@ -67,31 +67,30 @@ check_distro() {
     fi
 }
 
-
 lock_pkg() {
     for package in $1; do
-	    if [[ "$pkg_manager" == "apt" ]]; then
+        if [[ "$pkg_manager" == "apt" ]]; then
             run_cmd apt-mark hold $package
-	    elif [[ "$pkg_manager" == "yum" ]]; then
-	        run_cmd yum versionlock add $package
-        elif [[	"$pkg_manager" == "zypper" ]]; then
-	        run_cmd zypper addlock $package
-	    else
-	        echo "error: non-matching condition"
+        elif [[ "$pkg_manager" == "yum" ]]; then
+            run_cmd yum versionlock add $package
+        elif [[ "$pkg_manager" == "zypper" ]]; then
+            run_cmd zypper addlock $package
+        else
+            echo "error: non-matching condition"
         fi
     done
 }
 
 unlock_pkg() {
     for package in $1; do
-	    if [[ "$pkg_manager" == "apt" ]]; then
+        if [[ "$pkg_manager" == "apt" ]]; then
             run_cmd apt-mark unhold $package
-	    elif [[ "$pkg_manager" == "yum" ]]; then
-	        run_cmd yum versionlock delete $package
-        elif [[	"$pkg_manager" == "zypper" ]]; then
-	        run_cmd zypper removelock $package
-	    else
-	        echo "error: non-matching condition"
+        elif [[ "$pkg_manager" == "yum" ]]; then
+            run_cmd yum versionlock delete $package
+        elif [[ "$pkg_manager" == "zypper" ]]; then
+            run_cmd zypper removelock $package
+        else
+            echo "error: non-matching condition"
         fi
     done
 }
@@ -102,75 +101,79 @@ unlock_all_pkg() {
     elif [[ "$pkg_manager" == "yum" ]]; then
         run_cmd yum versionlock clear
     elif [[ "$pkg_manager" == "zypper" ]]; then
-	    # zypper removelock $(zypper locks | grep '^[[:digit:]]' | awk '{print $3}')
+        # zypper removelock $(zypper locks | grep '^[[:digit:]]' | awk '{print $3}')
         run_cmd zypper removelock $(zypper --xmlout locks | grep -o '<name>.*</name>' | sed -e 's/<name>\(.*\)<\/name>/\1/g')
     else
-	    echo "error: non-matching condition"
-    fi	
+        echo "error: non-matching condition"
+    fi
 }
 
 while :; do
     case $1 in
-        -h|--help)
-            show_help
-            exit
-            ;;
+    -h | --help)
+        show_help
+        exit
+        ;;
 
-        -l|--lock-pkg)
-	        check_distro
-    		echo "locking package(s)"
-	        if [[ -n $2 ]]; then
-		        if is_url $2; then
-			    echo "fetching package's list from $2"
-			    packages=$(curl_url $2)
-			    lock_pkg "$packages"
-		        else
-			packages=""
-			# while [[ $2 ]] && ! [[ "$2" =~ ^-.*$ ]]; do packages="$packages $2"; shift; done
-			while [[ $2 ]] && echo $2 | grep --quiet --ignore-case --invert-match '^-.*$'; do packages="$packages $2"; shift; done
-	                lock_pkg "$packages"
-		        fi
-	        else
-                echo 'error: "--lock-pkg" requires a non-empty option argument'
+    -l | --lock-pkg)
+        check_distro
+        echo "locking package(s)"
+        if [[ -n $2 ]]; then
+            if is_url $2; then
+                echo "fetching package's list from $2"
+                packages=$(curl_url $2)
+                lock_pkg "$packages"
+            else
+                packages=""
+                # while [[ $2 ]] && ! [[ "$2" =~ ^-.*$ ]]; do packages="$packages $2"; shift; done
+                while [[ $2 ]] && echo $2 | grep --quiet --ignore-case --invert-match '^-.*$'; do
+                    packages="$packages $2"
+                    shift
+                done
+                lock_pkg "$packages"
             fi
-	        break
-            ;;
+        else
+            echo 'error: "--lock-pkg" requires a non-empty option argument'
+        fi
+        break
+        ;;
 
-        -u|--unlock-pkg)
-	        check_distro
-    		echo "unlocking package(s)"
-	        if [[ -n $2 ]]; then
-		        if is_url $2; then
-			    echo "fetching package's list from $2"
-			    packages=$(curl_url $2)
-		            unlock_pkg "$packages"
-		        else
-			packages=""
-                        # while [[ $2 ]] && ! [[ "$2" =~ ^-.*$ ]]; do packages="$packages $2"; shift; done
-			while [[ $2 ]] && echo $2 | grep --quiet --ignore-case --invert-match '^-.*$'; do packages="$packages $2"; shift; done
-	                unlock_pkg "$packages"
-		        fi
-	        else
-                echo 'error: "--unlock-pkg" requires a non-empty option argument'
+    -u | --unlock-pkg)
+        check_distro
+        echo "unlocking package(s)"
+        if [[ -n $2 ]]; then
+            if is_url $2; then
+                echo "fetching package's list from $2"
+                packages=$(curl_url $2)
+                unlock_pkg "$packages"
+            else
+                packages=""
+                # while [[ $2 ]] && ! [[ "$2" =~ ^-.*$ ]]; do packages="$packages $2"; shift; done
+                while [[ $2 ]] && echo $2 | grep --quiet --ignore-case --invert-match '^-.*$'; do
+                    packages="$packages $2"
+                    shift
+                done
+                unlock_pkg "$packages"
             fi
-	        break
-            ;;
+        else
+            echo 'error: "--unlock-pkg" requires a non-empty option argument'
+        fi
+        break
+        ;;
+    
+    -U | --unlock-all-pkg)
+        check_distro
+        echo "unlocking all packages"
+        unlock_all_pkg
+        shift
+        break
+        ;;
 
-
-	    -U|--unlock-all-pkg)
-	        check_distro
-    		echo "unlocking all packages"
-	        unlock_all_pkg
-	        shift
-	        break
-	        ;;
-
-        *)
-	        echo "error: unknow option"
-	        show_help
-            exit
-            ;;
+    *)
+        echo "error: unknow option"
+        show_help
+        exit
+        ;;
     esac
     shift
 done
-
